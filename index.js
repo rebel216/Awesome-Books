fff398037a956f2115b0c290f2b85d7d5eb3fd0b
rebel216 <neerajbhardwajpkt@gmail.com>
2022-05-26 03:34:34 +0530

    Added navbar and lnks

--- index.js
+++ index.js
@@ -34,17 +34,17 @@ function getBook() {
   const Title = document.getElementById('bookTitle').value;
   const Author = document.getElementById('bookAuthor').value;
   const book = new Book(Title, Author);
-  document.getElementById('bookTitle').value = '';
-  document.getElementById('bookAuthor').value = '';
+  Title.value = '';
+  Author.value = '';
   return book;
 }
 
 function CreateUIBookList(bookOBJ) {
-  const BookListUi = document.getElementById('Book-List');
-  const bookList = document.createElement('div');
-  bookList.classList.add('list');
+  const BookListUi = document.getElementById('Bookcollection');
+  const bookList = document.createElement('li');
+  bookList.classList.add('book-item');
   bookList.setAttribute('id', bookOBJ.id);
-  bookList.innerHTML = `<p>${bookOBJ.Title} <br/> by ${bookOBJ.Author}</p>`;
+  bookList.innerHTML = `${bookOBJ.Title}by ${bookOBJ.Author}`;
   const deleteBook = document.createElement('button');
   deleteBook.innerHTML = 'Remove';
   deleteBook.addEventListener('click', () => booklist.removeBook(bookOBJ.id));
@@ -56,6 +56,8 @@ AddBooks.addEventListener('submit', (e) => {
   const newbook = getBook();
   booklist.addbook(newbook);
   CreateUIBookList(newbook);
+  document.getElementById('bookAuthor').value = '';
+  document.getElementById('bookTitle').value = '';
   e.preventDefault();
 });
 
@@ -68,3 +70,48 @@ window.onload = () => {
 
   booklist.data.forEach((book) => CreateUIBookList(book));
 };
+
+// eslint-disable-next-line no-unused-vars
+function display(section) {
+  const BookList = document.getElementById('Bookcollection');
+  const Form = document.getElementById('addBooks');
+  const Contact = document.getElementById('Contactinfo');
+  const heading = document.getElementById('title');
+  const linkAllBooks = document.getElementById('link');
+  const linkAddBook = document.getElementById('newBook');
+  const linkContact = document.getElementById('Contact');
+
+  switch (section) {
+    case 'Bookcollection':
+      BookList.style.display = 'grid';
+      linkAllBooks.style.color = 'blue';
+      linkAddBook.style.color = 'black';
+      linkContact.style.color = 'black';
+      Form.style.display = 'none';
+      Contact.style.display = 'none';
+      heading.innerHTML = 'All Awesome Books';
+      break;
+
+    case 'addBooks':
+      BookList.style.display = 'none';
+      Form.style.display = 'flex';
+      linkAllBooks.style.color = 'black';
+      linkAddBook.style.color = 'blue';
+      linkContact.style.color = 'black';
+      Contact.style.display = 'none';
+      heading.innerHTML = 'Add a New Book';
+      break;
+
+    case 'Contactinfo':
+      BookList.style.display = 'none';
+      Form.style.display = 'none';
+      linkAllBooks.style.color = 'black';
+      linkAddBook.style.color = 'black';
+      linkContact.style.color = 'blue';
+      Contact.style.display = 'block';
+      heading.innerHTML = 'Contact Information';
+      break;
+
+    default: break;
+  }
+}
