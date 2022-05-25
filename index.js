0111e4a5d05335253d3712c8fccaf5ad3d936b78
rebel216 <neerajbhardwajpkt@gmail.com>
2022-05-26 03:31:58 +0530

    Added navbar and lnks

--- index.js
+++ index.js
@@ -1,75 +1,117 @@
-const initialData = [
-  {
-    id: '176543',
-    Title: "Santa's Apprentice",
-    Author: 'Nicolle Plackstone',
-  },
-  {
-    id: '223244556',
-    Title: 'History Is Made at Night',
-    Author: 'Eduard Ekell',
-  },
-  {
-    id: '3324',
-    Title: 'Hercules',
-    Author: 'Ofelia Lockless',
-  },
-];
-const addBooks = document.getElementById('addBook-btn');
+/* eslint-disable max-classes-per-file */
 
-let BookList = [];
+const AddBooks = document.getElementById('addBooks');
 
-function addBook(book) {
-  BookList.push(book);
-  localStorage.setItem('bookData', JSON.stringify(BookList || '[]'));
+class BookList {
+  constructor() {
+    this.data = [];
+  }
+
+  addbook(book) {
+    this.data.push(book);
+    localStorage.setItem('bookdata', JSON.stringify(this.data));
+  }
+
+  removeBook(id) {
+    const book = document.getElementById(id);
+    book.remove();
+    this.data = this.data.filter((bookObj) => bookObj.id !== id);
+    localStorage.setItem('bookdata', JSON.stringify(this.data));
+  }
 }
 
-function Book(Title, Author) {
-  this.Title = Title;
-  this.Author = Author;
-  this.id = Math.random();
+class Book {
+  constructor(Title, Author) {
+    this.Title = Title;
+    this.Author = Author;
+    this.id = Math.random();
+  }
 }
+
+const booklist = new BookList();
 
 function getBook() {
   const Title = document.getElementById('bookTitle').value;
   const Author = document.getElementById('bookAuthor').value;
   const book = new Book(Title, Author);
-  document.getElementById('bookTitle').value = '';
-  document.getElementById('bookAuthor').value = '';
+  Title.value = '';
+  Author.value = '';
   return book;
 }
 
 function CreateUIBookList(bookOBJ) {
-  const BookListUi = document.getElementById('book-list');
-  const bookList = document.createElement('div');
-  bookList.classList.add('list');
+  const BookListUi = document.getElementById('Bookcollection');
+  const bookList = document.createElement('li');
+  bookList.classList.add('book-item');
   bookList.setAttribute('id', bookOBJ.id);
-  bookList.innerHTML = `<p>${bookOBJ.Title} <br/> by ${bookOBJ.Author}</p>`;
+  bookList.innerHTML = `${bookOBJ.Title}by ${bookOBJ.Author}`;
   const deleteBook = document.createElement('button');
   deleteBook.innerHTML = 'Remove';
-  deleteBook.addEventListener('click', () => {
-    const book = document.getElementById(bookOBJ.id);
-    book.remove();
-    BookList = BookList.filter((book) => book.id !== bookOBJ.id);
-    localStorage.setItem('bookData', JSON.stringify(BookList));
-  });
+  deleteBook.addEventListener('click', () => booklist.removeBook(bookOBJ.id));
   bookList.appendChild(deleteBook);
   BookListUi.appendChild(bookList);
 }
 
-addBooks.addEventListener('click', () => {
-  const newBook = getBook();
-  addBook(newBook);
-  CreateUIBookList(newBook);
+AddBooks.addEventListener('submit', (e) => {
+  const newbook = getBook();
+  booklist.addbook(newbook);
+  CreateUIBookList(newbook);
+  document.getElementById('bookAuthor').value = '';
+  document.getElementById('bookTitle').value = '';
+  e.preventDefault();
 });
 
 window.onload = () => {
-  const bookList = JSON.parse(localStorage.getItem('bookData' || '[]'));
-  if (bookList === null) {
-    localStorage.setItem('bookData', JSON.stringify(initialData));
+  booklist.data = JSON.parse(localStorage.getItem('bookdata' || '[]'));
+  if (booklist.data === null) {
+    booklist.data = [];
+    return;
   }
-  localStorage.setItem('bookData', JSON.stringify(bookList));
-  for (let i = 0; i < bookList.length; i += 1) {
-    CreateUIBookList(bookList[i]);
-  }
+
+  booklist.data.forEach((book) => CreateUIBookList(book));
 };
+
+// eslint-disable-next-line no-unused-vars
+function display(section) {
+  const BookList = document.getElementById('Bookcollection');
+  const Form = document.getElementById('addBooks');
+  const Contact = document.getElementById('Contactinfo');
+  const heading = document.getElementById('title');
+  const linkAllBooks = document.getElementById('link');
+  const linkAddBook = document.getElementById('newBook');
+  const linkContact = document.getElementById('Contact');
+
+  switch (section) {
+    case 'Bookcollection':
+      BookList.style.display = 'grid';
+      linkAllBooks.style.color = 'orange';
+      linkAddBook.style.color = 'black';
+      linkContact.style.color = 'black';
+      Form.style.display = 'none';
+      Contact.style.display = 'none';
+      heading.innerHTML = 'All Awesome Books';
+      break;
+
+    case 'addBooks':
+      BookList.style.display = 'none';
+      Form.style.display = 'flex';
+      linkAllBooks.style.color = 'black';
+      linkAddBook.style.color = 'orange';
+      linkContact.style.color = 'black';
+      Contact.style.display = 'none';
+      heading.innerHTML = 'Add a New Book';
+      break;
+
+    case 'Contactinfo':
+      BookList.style.display = 'none';
+      Form.style.display = 'none';
+      linkAllBooks.style.color = 'black';
+      linkAddBook.style.color = 'orange';
+      linkContact.style.color = 'blue';
+      Contact.style.display = 'block';
+      heading.innerHTML = 'Contact Information';
+      break;
+
+    default: break;
+  }
+}
